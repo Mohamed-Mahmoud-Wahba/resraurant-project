b752511afe61099173fcbdda393a0263d1292e4b
mohamed wahba <mohamed.wahba@baianat.com>
2023-01-03 15:31:48 +0200

    first commit

---
+++ components/Nav.vue
@@ -0,0 +1,23 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <nuxt-link class="navbar-brand" to="/">Restaurantly</nuxt-link>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <nuxt-link class="nav-link active" aria-current="page" to="/">Home</nuxt-link>
+              </li>
+              <li class="nav-item">
+                <nuxt-link class="nav-link" to="/resraurants">Resraurant</nuxt-link>
+              </li>
+
+            </ul>
+          </div>
+        </div>
+      </nav>
+</template>
+<style lang="scss" scoped>
+.ml-auto{
+    margin-left: auto;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <Html :lang="dynamic > 50 ? 'ar-EG' : 'en-US'">
+    <Head>
+      <Title>Restaurantly</Title>
+      <Meta name="description" content="My page is cool" />
+      <Link
+        rel="preload"
+        href="https://www.iconpacks.net/icons/2/free-sad-face-icon-2691-thumb.png"
+        as="script"
+      />
+    </Head>
+  </Html>
+  <div class="container">
+    <h1>Welcome To Restaurantly</h1>
+    <nuxt-link to="/resraurants">Go To Resraurants</nuxt-link>
+  </div>
+</template>
+<style lang="scss" scoped>
+.container {
+  text-align: center;
+  padding-top: 100px;
+}
+</style>
